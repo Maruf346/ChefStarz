698c193084b9006a49fc7898f105e93c42cf8b2b
Maruf346 <maruf.bshs@gmail.com>
2026-06-28 15:38:30 +0600

    removed ssl for email to make sure this works fine or not

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -273,7 +273,7 @@ else:
     EMAIL_HOST = config('EMAIL_HOST', default='smtp.gmail.com')
     EMAIL_PORT = config('EMAIL_PORT', default=587)
     EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True)
-    EMAIL_USE_SSL = config('EMAIL_USE_SSL', default=False)
+    # EMAIL_USE_SSL = config('EMAIL_USE_SSL', default=False)
     EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
     EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
     DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL', default=EMAIL_HOST_USER)
